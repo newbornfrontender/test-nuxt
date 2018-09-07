37eec7ba6777909382a43d1cf51fd5722289ca67
Infant Frontender <newbornfrontender@gmail.com>
2018-09-07 13:11:27 +0300

    Добавил основную страницу

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>{{ msg }}</div>
+    <ul>
+      <li
+        v-for="item of list"
+        v-bind:key="item.id"
+      >{{ item.text }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    msg: 'Message',
+    list: [{
+      text: '1'
+    }, {
+      text: '2'
+    }]
+  })
+}
+</script>
