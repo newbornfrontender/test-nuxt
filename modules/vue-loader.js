9bd43908c2e49ddd16c406e69cd959004091b8b4
Infant Frontender <newbornfrontender@gmail.com>
2018-09-08 11:41:35 +0300

    Переписал код

--- modules/vue-loader.js
+++ modules/vue-loader.js
@@ -1,10 +1,8 @@
 module.exports = function() {
   this.extendBuild(config => {
-    const vueLoader = config.module.rules.find(
+    config.module.rules.find(
       rule => rule.loader === 'vue-loader'
-    )
-
-    vueLoader.options.cssModules = {
+    ).options.cssModules = {
       localIdentName: '[path]-[name]__[local]_[hash:base64:5]',
       camelCase: 'only'
     }
