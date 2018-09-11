234d278419911a09f68ae4dd480ae6f8c5ddbfb5
Infant Frontender <newbornfrontender@gmail.com>
2018-09-11 09:31:57 +0300

    Изменил структуру и имена модулей

--- modules/vue-loader.js
+++ modules/vue-loader.js
@@ -1,9 +1,13 @@
 module.exports = function() {
   this.extendBuild(config => {
-    config.module.rules.find(
+    const loader = config.module.rules.find(
       rule => rule.loader === 'vue-loader'
-    ).options.cssModules = {
-      localIdentName: '[path]-[name]__[local]_[hash:base64:5]',
+    )
+
+    loader.options.cssModules = {
+      // localIdentName: '[path]-[name]__[local]_[hash:base64:3]',
+      // localIdentName: '[name]__[local]_[hash:base64:3]',
+      localIdentName: '[name]__[local]_[hash:hex:5]',
       camelCase: 'only'
     }
   })
