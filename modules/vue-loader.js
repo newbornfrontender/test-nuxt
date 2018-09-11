093513f8241b6ddd5c1f4e3ec6ae8ebde4a5ed1d
Infant Frontender <newbornfrontender@gmail.com>
2018-09-11 09:32:39 +0300

    Убрал комментарии

--- modules/vue-loader.js
+++ modules/vue-loader.js
@@ -5,8 +5,6 @@ module.exports = function() {
     )
 
     loader.options.cssModules = {
-      // localIdentName: '[path]-[name]__[local]_[hash:base64:3]',
-      // localIdentName: '[name]__[local]_[hash:base64:3]',
       localIdentName: '[name]__[local]_[hash:hex:5]',
       camelCase: 'only'
     }
