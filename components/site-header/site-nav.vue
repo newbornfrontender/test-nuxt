a69cf1dbdefe60666b586622e9a84c27db9945b9
Infant Frontender <newbornfrontender@gmail.com>
2018-09-13 11:17:27 +0300

    Добавил компонент с навигацией для site-header

---
+++ components/site-header/site-nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link to="/">Главная</nuxt-link>
+      </li>
+
+      <li>
+        <nuxt-link to="/test">Тест</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
