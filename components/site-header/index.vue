6e83b40d83ba4a43be1b286ceada8f065198939a
Infant Frontender <newbornfrontender@gmail.com>
2018-09-13 11:16:37 +0300

    Добавил кастомный header

---
+++ components/site-header/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div v-bind:class="$style.logo">
+      <span v-bind:class="$style.logoInner">Logo</span>
+    </div>
+
+    <site-nav />
+  </header>
+</template>
+
+<script>
+export default {
+  components: {
+    'site-nav': () => import('./site-nav')
+  }
+}
+</script>
+
+<style lang="postcss" module>
+.logo {
+  background-color: green;
+
+  &Inner {
+    color: orange;
+  }
+}
+</style>
