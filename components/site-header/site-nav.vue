3b85349288b394904eef84a02c3fa1f8ae9e311b
Infant Frontender <newbornfrontender@gmail.com>
2018-09-13 11:29:40 +0300

    Вывел список через цикл

--- components/site-header/site-nav.vue
+++ components/site-header/site-nav.vue
@@ -1,13 +1,27 @@
 <template>
   <nav>
     <ul>
-      <li>
-        <nuxt-link to="/">Главная</nuxt-link>
-      </li>
-
-      <li>
-        <nuxt-link to="/test">Тест</nuxt-link>
+      <li
+        v-for="item of list"
+        v-bind:key="item.id"
+      >
+        <nuxt-link v-bind:to="item.path">{{ item.title }}</nuxt-link>
       </li>
     </ul>
   </nav>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    /* prettier-ignore */
+    list: [{
+      title: 'Главная',
+      path: '/'
+    }, {
+      title: 'Тест',
+      path: '/test'
+    }]
+  })
+}
+</script>
