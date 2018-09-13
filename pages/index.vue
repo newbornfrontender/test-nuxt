d206ce0376601bc676a464971d0ddd1dbe542d36
Infant Frontender <newbornfrontender@gmail.com>
2018-09-13 11:12:39 +0300

    Добавил мету и слегка изменил структуру шаблона

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,25 @@
 <template>
-  <div>
-    <h1 v-bind:class="[$style.title, $style.titleBig]">{{ msg }}</h1>
-    <ul>
-      <li
-        v-for="item of list"
-        v-bind:key="item.id"
-      >{{ item.text }}</li>
-    </ul>
-  </div>
+  <section>
+    <h1>{{ msg }}</h1>
+  </section>
 </template>
 
 <script>
 export default {
   data: () => ({
-    msg: 'Message',
-    /* prettier-ignore */
-    list: [{
-      text: '1'
-    }, {
-      text: '2'
-    }]
-  })
+    msg: 'Главная страница'
+  }),
+
+  head() {
+    return {
+      title: this.msg,
+      /* prettier-ignore */
+      meta: [{
+        hid: 'index-description',
+        name: 'description',
+        content: this.msg
+      }]
+    }
+  }
 }
 </script>
-
-<style module>
-.title {
-  color: red;
-}
-
-.titleBig {
-  font-size: 30px;
-}
-</style>
