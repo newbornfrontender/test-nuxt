ad9658d4d1a05ad0fce29fd13a0b6693c0daaa3d
Infant Frontender <newbornfrontender@gmail.com>
2018-09-13 11:14:35 +0300

    Добавил стандартный шаблон

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <site-header />
+
+    <site-main />
+
+    <site-footer />
+  </section>
+</template>
+
+<script>
+export default {
+  components: {
+    'site-header': () => import('~/components/site-header'),
+    'site-main': () => import('~/components/site-main'),
+    'site-footer': () => import('~/components/site-footer')
+  }
+}
+</script>
