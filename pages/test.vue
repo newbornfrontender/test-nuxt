7c6a62d37840a341e93c91ec9d80331b53d6a415
Infant Frontender <newbornfrontender@gmail.com>
2018-09-13 11:11:45 +0300

    Добавил тестовую страницу

---
+++ pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h1>{{ msg }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    msg: 'Тестовая страница'
+  }),
+
+  head() {
+    return {
+      title: this.msg,
+      /* prettier-ignore */
+      meta: [{
+        hid: 'test-description',
+        name: 'description',
+        content: this.msg
+      }]
+    }
+  }
+}
+</script>
